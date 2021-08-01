1e1a4b0eb5d5b880b38225b1bdbb8b09cc5f9f50
gritsiem <ghritachi.mahajani@gmail.com>
2021-08-01 12:12:02 +0000

    Added car models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,20 @@
 from django.contrib import admin
 # from .models import related models
-
+from .models import CarMake,CarModel
 
 # Register your models here.
 
 # CarModelInline class
-
+class CarModelInline(admin.StackedInline):
+   model = CarModel
 # CarModelAdmin class
 
+
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    model = CarMake
+    inline = [CarModelInline]
 
 # Register models here
+admin.site.register(CarMake,CarMakeAdmin)
+admin.site.register(CarModel)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -10,6 +10,13 @@ from django.utils.timezone import now
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
 
+class CarMake(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200) 
+    def __str__(self):
+        return self.name
+        
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
@@ -20,6 +27,24 @@ from django.utils.timezone import now
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
 
+
+class CarModel(models.Model):
+    SUV = "SUV"
+    SEDAN = "Sedan"
+    WAGON = "WAGON"
+    TYPE_OPTIONS=[
+        ("SUV", SUV), ("Sedan",SEDAN), ("WAGON",WAGON)
+    ]
+    id = models.AutoField(primary_key=True)
+    make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    dealerId = models.PositiveIntegerField()
+    name = models.CharField(max_length= 50)
+    Type = models.CharField(
+        choices=TYPE_OPTIONS, max_length=5, default=SEDAN
+    )
+    year = models.DateField()
+    def __str__(self):
+        return self.name
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 
